3df91a533c440c08025b720ec938f4ee8da0aad8
Kleshny77 <samsonovartem00@gmail.com>
2026-01-16 23:52:15 +0300

    Initial Commit

---
+++ cstatiWarehouse/cstatiWarehouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  cstatiWarehouseApp.swift
+//  cstatiWarehouse
+//
+//  Created by Artem Samsonov on 16.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct cstatiWarehouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cstatiWarehouseTests/cstatiWarehouseTests.swift
@@ -0,0 +1,17 @@
+//
+//  cstatiWarehouseTests.swift
+//  cstatiWarehouseTests
+//
+//  Created by Artem Samsonov on 16.01.2026.
+//
+
+import Testing
+@testable import cstatiWarehouse
+
+struct cstatiWarehouseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
